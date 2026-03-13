b328e3e702bc530313c3f4bdf495e046872556fb
김희성 <forusastrid@gmail.com>
2026-03-13 10:17:04 +0900

    Update Ridge and Lasso regression parameters and scores

--- summary/day6.py
+++ summary/day6.py
@@ -1,21 +1,21 @@
-#32
+#35
 from sklearn.linear_model import Ridge
 
 ridge = Ridge().fit(X_train, y_train)
 print("훈련 세트 점수: {:.2f}".format(ridge.score(X_train, y_train)))
 print("테스트 세트 점수: {:.2f}".format(ridge.score(X_test, y_test)))
 
-#33
+#36
 ridge10 = Ridge(alpha=10).fit(X_train, y_train)
 print("훈련 세트 점수: {:.2f}".format(ridge10.score(X_train, y_train)))
 print("테스트 세트 점수: {:.2f}".format(ridge10.score(X_test, y_test)))
 
-#34
+#37
 ridge01 = Ridge(alpha=0.1).fit(X_train, y_train)
 print("훈련 세트 점수: {:.2f}".format(ridge01.score(X_train, y_train)))
 print("테스트 세트 점수: {:.2f}".format(ridge01.score(X_test, y_test)))
 
-#35
+#38
 plt.plot(ridge10.coef_,'^', label="Ridge alpha=10")
 plt.plot(ridge.coef_,'s', label="Ridge alpha=1")
 plt.plot(ridge01.coef_,'v', label="Ridge alpha=0.1")
@@ -30,10 +30,10 @@ plt.ylim(-25, 25)
 plt.legend()
 plt.show()
 
-#36
+#39
 mglearn.plots.plot_ridge_n_samples()
 
-#37
+#40
 from sklearn>linear_model import Lasso
 
 lasso = Lasso().fit(X_train, y_train)
@@ -41,19 +41,19 @@ print("훈련 세트 점수: {:.2f}".format(lasso.score(X_train, y_train)))
 print("테스트 세트 점수: {:.2f}".format(lasso.score(X_tet, y_test)))
 print("사용한 특성의 개수", np.sum(lasso.coef_ !=0))
 
-#38
+#41
 lasso001 = Lasso(alpha=0.01, max_iter=50000).fit(X_train, y_train)
 print("훈련 세트 점수: {:.2f}".format(lasso001.score(X_train, y_train)))
 print("테스트 세트 점수: {:.2f}".format(lasso001.score(X_tet, y_test)))
 print("사용한 특성의 개수", np.sum(lasso001.coef_ !=0))
 
-#39
+#42
 lasso00001 = Lasso(alpha=0.0001, max_iter=50000).fit(X_train, y_train)
 print("훈련 세트 점수: {:.2f}".format(lasso00001.score(X_train, y_train)))
 print("테스트 세트 점수: {:.2f}".format(lasso00001.score(X_tet, y_test)))
 print("사용한 특성의 개수", np.sum(lasso00001.coef_ !=0))
 
-#40
+#43
 plt.plot(lasso.coef_,'s', label="Lasso alpha=1")
 plt.plot(lasso001.coef_,'^', label="Lasso alpha=0.01")
 plt.plot(lasso00001.coef_,'v', label="Lasso alpha=0.0001")
